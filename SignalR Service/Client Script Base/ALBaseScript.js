59d46a0640c9f304dc9b44404e51bb673b75e94a
Spadar <j.paigeNH@gmail.com>
2018-11-16 22:06:55 -0500

    Initial Files

---
+++ SignalR Service/Client Script Base/ALBaseScript.js
@@ -0,0 +1,35 @@
+﻿//Get jquery.signalR-2.3.0.js from the source project "Scripts" Folder. 
+//Save as SignalR in AL. 
+load_code("SignalR");
+
+var connected = false;
+
+$.connection.hub.url = 'http://localhost:8080/signalr';
+
+var connection = $.hubConnection();
+
+connection.url = 'http://localhost:8080/signalr';
+
+var hubProxy = connection.createHubProxy('signalRServiceHub');
+
+connection.start().done(function () {
+    game_log("Connected");
+    connected = true;
+}).fail(function () {
+    game_log('Could not Connect!');
+});
+
+var lastPing;
+hubProxy.on('Pong', function (message) {
+    game_log("Server responded in " + (new Date() - lastPing) + "ms.");
+    lastPing = null;
+});
+
+setInterval(function () {
+    if (connected) {
+        if (lastPing == null) {
+            hubProxy.invoke('Ping');
+            lastPing = new Date();
+        }
+    }
+}, 40);
---
+++ SignalR Service/SignalRServiceHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR_Service
+{
+    public class SignalRServiceHub : Hub
+    {
+        public void Ping()
+        {
+            Clients.Caller.Pong(DateTime.Now);
+        }
+    }
+}
